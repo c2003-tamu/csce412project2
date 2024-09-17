92ef1ddc304861516c0d5a9b0a4022ae4010183e
c2003-tamu <c2003@tamu.edu>
2024-09-17 12:00:48 -0500

    init comments

--- init.sh
+++ init.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
-
+# Update and upgrade
 sudo apt update && sudo apt upgrade -y
-
 sudo apt install docker.io -y
 
+# Pull nginx image
 sudo docker pull nginx
 
+# Start the containers with modified HTML before starting each one
 sed -i "s|<strong>.*</strong>|<strong>8080</strong>|g" ./nginx_html/index.html
-
 sudo docker run --name mynginx1 -d -p 8080:80 -v ~/csce412project2/nginx_html:/usr/share/nginx/html nginx
 
 sed -i "s|<strong>.*</strong>|<strong>8081</strong>|g" ./nginx_html/index.html
@@ -22,4 +21,5 @@ sudo docker run --name mynginx4 -d -p 8083:80 -v ~/csce412project2/nginx_html:/u
 sed -i "s|<strong>.*</strong>|<strong>8084</strong>|g" ./nginx_html/index.html
 sudo docker run --name mynginx5 -d -p 8084:80 -v ~/csce412project2/nginx_html:/usr/share/nginx/html nginx
 
+# Restore original value
 sed -i "s|<strong>.*</strong>|<strong>%%number%%</strong>|g" ./nginx_html/index.html
