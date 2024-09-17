0bf359764bc4ccd6cc4ced4135f6c58be7c9f5db
c2003-tamu <c2003@tamu.edu>
2024-09-17 11:50:53 -0500

    add number to different instances

--- init.sh
+++ init.sh
@@ -6,12 +6,20 @@ sudo apt install docker.io -y
 
 sudo docker pull nginx
 
+sed -i "s|<strong>.*</strong>|<strong>8080</strong>|g" ./nginx_html/index.html
+
 sudo docker run --name mynginx1 -d -p 8080:80 -v ~/csce412project2/nginx_html:/usr/share/nginx/html nginx
 
+sed -i "s|<strong>.*</strong>|<strong>8081</strong>|g" ./nginx_html/index.html
 sudo docker run --name mynginx2 -d -p 8081:80 -v ~/csce412project2/nginx_html:/usr/share/nginx/html nginx
 
+sed -i "s|<strong>.*</strong>|<strong>8082</strong>|g" ./nginx_html/index.html
 sudo docker run --name mynginx3 -d -p 8082:80 -v ~/csce412project2/nginx_html:/usr/share/nginx/html nginx
 
+sed -i "s|<strong>.*</strong>|<strong>8083</strong>|g" ./nginx_html/index.html
 sudo docker run --name mynginx4 -d -p 8083:80 -v ~/csce412project2/nginx_html:/usr/share/nginx/html nginx
 
+sed -i "s|<strong>.*</strong>|<strong>8084</strong>|g" ./nginx_html/index.html
 sudo docker run --name mynginx5 -d -p 8084:80 -v ~/csce412project2/nginx_html:/usr/share/nginx/html nginx
+
+sed -i "s|<strong>.*</strong>|<strong>%%number%%</strong>|g" ./nginx_html/index.html
