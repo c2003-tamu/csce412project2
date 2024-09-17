0e4bd050e447795c76c8361b67b0b4ed4478bc07
c2003-tamu <c2003@tamu.edu>
2024-09-17 11:38:46 -0500

    5 fold init script

--- init.sh
+++ init.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
 
-sudo apt install docker.io
+sudo apt install docker.io -y
 
 sudo docker pull nginx
 
-sudo docker run --name mynginx -d -p 80:80 -v ~/csce412project2/nginx_html:/usr/share/nginx/html nginx
+sudo docker run --name mynginx1 -d -p 8080:80 -v ~/csce412project2/nginx_html:/usr/share/nginx/html nginx
+
+sudo docker run --name mynginx2 -d -p 8081:80 -v ~/csce412project2/nginx_html:/usr/share/nginx/html nginx
+
+sudo docker run --name mynginx3 -d -p 8082:80 -v ~/csce412project2/nginx_html:/usr/share/nginx/html nginx
+
+sudo docker run --name mynginx4 -d -p 8083:80 -v ~/csce412project2/nginx_html:/usr/share/nginx/html nginx
+
+sudo docker run --name mynginx5 -d -p 8084:80 -v ~/csce412project2/nginx_html:/usr/share/nginx/html nginx
