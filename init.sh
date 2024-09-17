55dd04d5d241735548718ee8f27239bbb01d888b
c2003-tamu <c2003@tamu.edu>
2024-09-17 09:26:28 -0500

    rename init script

--- init.sh
+++ init.sh
@@ -1,44 +1,9 @@
 #!/bin/bash
 
-sudo apt-get update
+sudo apt update && sudo apt upgrade
 
-sudo apt-get install ca-certificates curl
-
-sudo install -m 0755 -d /etc/apt/keyrings
-
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-echo \
-
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update
-
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-sudo docker run hello-world
-
-sudo groupadd docker
-
-sudo gpasswd -a $USER docker
-
-newgrp docker
-
-docker run hello-world
-
-docker image ls
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-sudo chmod +x /usr/local/bin/docker-compose
-
-docker-compose --version
+sudo apt install docker.io
 
 sudo docker pull nginx
 
 sudo docker run --name mynginx -d -p 80:80 -v ~/csce412project2/nginx_html:/usr/share/nginx/html nginx
-
--- init2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo apt update && sudo apt upgrade
-
-sudo apt install docker.io
-
-sudo docker pull nginx
-
-sudo docker run --name mynginx -d -p 80:80 -v ~/csce412project2/nginx_html:/usr/share/nginx/html nginx
