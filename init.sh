d2d045a063291d233abaf7c5c0f0ca5b194153f0
c2003-tamu <c2003@tamu.edu>
2024-09-17 13:10:16 -0500

    use different files

--- init.sh
+++ init.sh
@@ -5,12 +5,12 @@ sudo apt install docker.io -y
 # Pull nginx image
 sudo docker pull nginx
 
-sudo docker run --name mynginx1 -d -p 8080:80 -v ~/csce412project2/nginx_html/8080.html:/usr/share/nginx/html nginx
+sudo docker run --name mynginx1 -d -p 8080:80 -v ~/csce412project2/nginx_html/8080.html:/usr/share/nginx/html/index.html nginx
 
-sudo docker run --name mynginx2 -d -p 8081:80 -v ~/csce412project2/nginx_html/8081.html:/usr/share/nginx/html nginx
+sudo docker run --name mynginx2 -d -p 8081:80 -v ~/csce412project2/nginx_html/8081.html:/usr/share/nginx/html/index.html nginx
 
-sudo docker run --name mynginx3 -d -p 8082:80 -v ~/csce412project2/nginx_html/8082.html:/usr/share/nginx/html nginx
+sudo docker run --name mynginx3 -d -p 8082:80 -v ~/csce412project2/nginx_html/8082.html:/usr/share/nginx/html/index.html nginx
 
-sudo docker run --name mynginx4 -d -p 8083:80 -v ~/csce412project2/nginx_html/8083.html:/usr/share/nginx/html nginx
+sudo docker run --name mynginx4 -d -p 8083:80 -v ~/csce412project2/nginx_html/8083.html:/usr/share/nginx/html/index.html nginx
 
-sudo docker run --name mynginx5 -d -p 8084:80 -v ~/csce412project2/nginx_html/8084.html:/usr/share/nginx/html nginx
+sudo docker run --name mynginx5 -d -p 8084:80 -v ~/csce412project2/nginx_html/8084.html:/usr/share/nginx/html/index.html nginx
