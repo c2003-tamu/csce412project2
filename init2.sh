dc5cf748759bf16304655aaf6d76ac08eb527191
c2003-tamu <c2003@tamu.edu>
2024-09-20 13:44:57 -0500

    tweak to init2

--- init2.sh
+++ init2.sh
@@ -16,14 +16,10 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo groupadd docker
-
-sudo gpasswd -a $USER docker
-
-newgrp docker
-
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
 docker-compose --version
+
+docker-compose up -d
