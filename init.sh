00447bbcf46812b11f7c6e6360a63acc33383842
c2003-tamu <c2003@tamu.edu>
2024-09-22 11:00:59 -0500

    y flags added to installation

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 
-sudo apt-get install ca-certificates curl
+sudo apt-get install ca-certificates curl -y
 
 sudo install -m 0755 -d /etc/apt/keyrings
 
@@ -14,7 +14,7 @@ echo \
 
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
